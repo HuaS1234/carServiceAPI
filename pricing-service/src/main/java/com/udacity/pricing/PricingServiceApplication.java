de1d44fdb6883f165b839328eed453c434f3353a
Hua Shi <huashi2630@gmail.com>
2021-04-01 15:15:42 -0700

    added Eureka

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,12 +2,16 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
